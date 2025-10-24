8b18016914576e830f74f620d7cd11f64d7a3924
Germanovct <germanovct@gmail.com>
2025-10-23 21:50:41 -0300

    modal

--- src/components/WelcomeModal.jsx
+++ src/components/WelcomeModal.jsx
@@ -23,7 +23,7 @@ export default function WelcomeModal() {
         <div className="modal-content bg-dark text-white text-center p-4">
           <h2 className="mb-3">TE DAMOS LA BIENVENIDA</h2>
           <p className="mb-3">
-            ESTA SEMANA FINDE XXL , JUEVES PRE FERIADO , VIERNES SE*TUP WIZARD , SABADO MINAU BDAY
+            ESTE VIERNES RADIO MINAU , TCQ 180º w/ PANDY - GERMANO - TUMAS , SABADO CERRADO POR VEDA ELECTORAL
           </p>
           <button
             className="btn-modern w-100"
